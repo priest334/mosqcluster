f5418569a4a10ebc7a6f43f0c16dd745df2a559e
pengheliang <penghl@tq.cn>
2020-02-06 21:16:38 +0800

    contact message

--- xauth/xauth.cpp
+++ xauth/xauth.cpp
@@ -3,11 +3,14 @@
 #include "route_new_token.h"
 #include "route_wxwork_message.h"
 #include "route_notify.h"
+#include "route_contact_notify.h"
 
 
 void XAuthRoutes::InitTable() {
 	Register(new RouteNewToken());
-	Register(new GetRouteWxWorkMessage());
-	Register(new PostRouteWxWorkMessage());
+	Register(new RouteWxWorkMessageVerify());
+	Register(new RouteWxWorkMessageHandler());
 	Register(new RouteNotify());
+	Register(new RouteContactVerify());
+	Register(new RouteContactHandler());
 }
